908a081a43c582426dd2badc2bbe19a58b464e12
haison12 <36688293+haison12@users.noreply.github.com>
2025-02-04 15:16:36 -0500

    started project with poetry

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='implementing classifier algoritmh based in naive bayes',
+    author='Haison-ai',
+    license='MIT',
+)
