6b3ef92350057ff78c4ed8efc3eb836acc753a99
haison12 <36688293+haison12@users.noreply.github.com>
2025-02-05 01:58:21 -0500

    creating class to upload into s3 bucket

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name="src",
     packages=find_packages(),
-    version='0.1.0',
-    description='implementing classifier algoritmh based in naive bayes',
-    author='Haison-ai',
-    license='MIT',
+    version="0.1.0",
+    description="implementing classifier algoritmh based in naive bayes",
+    author="Haison-ai",
+    license="MIT",
 )
---
+++ src/data/loader_s3.py
@@ -0,0 +1,40 @@
+import os
+
+import boto3
+
+
+class load_data:
+    def __init__(self, s3_name: str, s3_folder: str, region: str = "us-east-2"):
+        """
+        With this class gonna to upload the raw data in s3 bucket.
+        """
+        self.s3_name = s3_name
+        self.s3_folder = s3_folder
+        self.s3 = boto3.client("s3", region_name=region)
+
+    def load_s3(self, local_path: str):
+        """
+        Method to load the s3 bucket.
+        """
+        try:
+            if not os.path.exists(local_path):
+                print(f" file doesn't exist in: {local_path}")
+                return False
+            # cleating client in s3
+            file_name = os.path.basename(local_path)
+            s3_path = f"{self.s3_folder}/{file_name}"
+
+            self.s3.upload_file(local_path, self.s3_name, s3_path)
+            print(
+                f" files uploaded succesufully: {local_path} → s3://{self.s3_name}/{s3_path}"
+            )
+            return s3_path
+
+        except Exception as e:
+            print(f" Error to upload file: {e}")
+            return False
+
+
+if __name__ == "__main__":
+    uploading = load_data("movieclassifiers3", "raw")
+    uploading.load_s3("data/raw/users.dat")
